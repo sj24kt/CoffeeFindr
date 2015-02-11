73022e3093d66ee680168e6979adfae53fa422ba
Sherrie Jones <sj24kt@comcast.net>
2015-02-11 10:32:38 -0700

    completed app

---
+++ CoffeeFindr/CoffeePlace.m
@@ -0,0 +1,13 @@
+//
+//  CoffeePlace.m
+//  CoffeeFindr
+//
+//  Created by Sherrie Jones on 2/10/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import "CoffeePlace.h"
+
+@implementation CoffeePlace
+
+@end
